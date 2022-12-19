2014f4300d7100d7c93a205b8568eea006c6f6b7
Mateusz Ożóg <ozog.mat@o2.pl>
2022-12-19 22:12:24 +0100

    Fix eslint errors

--- src/views/home-page/home-page.jss.ts
+++ src/views/home-page/home-page.jss.ts
@@ -1,7 +1,7 @@
 import { Box } from '@mui/material';
 import styled from '@emotion/styled';
 
-export const HomePageContainer = styled(Box)(({ theme }) => ({
+export const HomePageContainer = styled(Box)({
   width: '100%',
   height: '100%',
-}));
+});
