d77d472523eead238cc1fb93697efaa578d02ccf
Mateusz Ożóg <ozog.mat@o2.pl>
2022-12-19 21:57:11 +0100

    Add react router to application, themes and mui library

---
+++ src/views/home-page/home-page.jss.ts
@@ -0,0 +1,7 @@
+import { Box } from '@mui/material';
+import styled from '@emotion/styled';
+
+export const HomePageContainer = styled(Box)(({ theme }) => ({
+  width: '100%',
+  height: '100%',
+}));
